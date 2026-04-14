2415530d27cbc8c83b57bd51f234420b739bba5b
Shuvo <suvo10.info@gmail.com>
2026-04-14 14:01:31 +0600

    bvjnl

---
+++ src/train.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import numpy as np
+import os
+import mlflow 
+import mlflow.sklearn
+from dotenv import load_dotenv
+import yaml 
+import dagshub
+
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score,f1_score,roc_auc_score,classification_report
+from xgboost import XGBClassifier
+from lightgbm import LGBMClassifier
+load_dotenv()
+
